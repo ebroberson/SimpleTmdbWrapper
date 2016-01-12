4194ed356e0e8a913098ab682b14af7998259f3b
Eric Brandon Roberson <me@ebroberson.com>
2016-01-12 15:29:46 -0600

    added unit tests

---
+++ SimpleTmdbWrapper.Test/MovieQueryTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTmdbWrapper.Queries;
+
+namespace SimpleTmdbWrapper.Test
+{
+    [TestClass]
+    public class MovieQueryTest
+    {
+        [TestMethod]
+        public void SearchMovie()
+        {
+        }
+    }
+}
---
+++ SimpleTmdbWrapper.Test/RateLimiterTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTmdbWrapper.Limiters;
+
+namespace SimpleTmdbWrapper.Test
+{
+    [TestClass]
+    public class RateLimiterTest
+    {
+        [TestMethod]
+        public void RateLimitWithColdTask()
+        {
+            var limiter = new TimeSpanRateLimiter(10, TimeSpan.FromSeconds(2D));
+            Duplicate(() => limiter.Limit(new Task(TaskToRun)), 30);
+        }
+
+        private void Duplicate(Action action, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                action();
+            }
+        }
+
+        private void TaskToRun()
+        {
+            Console.WriteLine(DateTime.UtcNow.ToString());
+        }
+    }
+}
